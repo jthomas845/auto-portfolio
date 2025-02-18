a0c2e342fc5eadcce60f5ca4cd251ceff9da1980
jthomas845 <54633261+jthomas845@users.noreply.github.com>
2025-02-18 09:40:00 -0500

    Update stackbit.config.ts -- added work as a model extension

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -19,6 +19,12 @@ const config = defineStackbitConfig({
             }
         })
     ],
+    modelExtensions: [
+    { name: "page", type: "page", urlPath: "/{slug}" },
+    { name: "post", type: "page", urlPath: "/blog/{slug}" },
+    { name: "work", type: "page", urlPath: "/work/{slug}" },
+    { name: "projects", type: "page", urlPath: "/projects/{slug}" }
+  ],
     presetSource: {
         type: 'files',
         presetDirs: ['./.stackbit/presets']
