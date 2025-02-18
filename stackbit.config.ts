3a1d61eb3336a78cb3604d239bdfad43b6ee902f
Jarius T <jetstreakmusic@gmail.com>
2025-02-18 11:53:27 -0500

    Revert "Update stackbit.config.ts -- added work as a model extension"
    This reverts commit a0c2e342fc5eadcce60f5ca4cd251ceff9da1980.

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -11,14 +11,14 @@ const config = defineStackbitConfig({
             rootPath: __dirname,
             contentDirs: ["content"],
             models: [
-            {
-              name: "Page",
-              // Define the model as a page model
-              type: "page",
-              urlPath: "/{slug}",
-              filePath: "content/pages/{slug}.md",
-              fields: [{ name: "title", type: "string", required: true }]
-            }
+                {
+                    name: "Page",
+                    // Define the model as a page model
+                    type: "page",
+                    urlPath: "/{slug}",
+                    filePath: "content/pages/{slug}.md",
+                    fields: [{ name: "title", type: "string", required: true }]
+                }
             ],
             assetsConfig: {
                 referenceType: 'static',
@@ -28,40 +28,6 @@ const config = defineStackbitConfig({
             }
         })
     ],
-    modelExtensions: [
-        { name: "page", type: "page", urlPath: "/{slug}" },
-        { name: "post", type: "page", urlPath: "/blog/{slug}" }
-    ],
-    siteMap: ({ documents, models }) => {
-    // 1. Filter all page models
-    const pageModels = models.filter((m) => m.type === "page")
-
-    return documents
-      // 2. Filter all documents which are of a page model
-      .filter((d) => pageModels.some(m => m.name === d.modelName))
-      // 3. Map each document to a SiteMapEntry
-      .map((document) => {
-        // Map the model name to its corresponding URL
-        const urlModel = (() => {
-            switch (document.modelName) {
-                case 'Page':
-                    return 'otherPage';
-                case 'Blog':
-                    return 'otherBlog';
-                default:
-                    return null;
-            }
-        })();
-
-        return {
-          stableId: document.id,
-          urlPath: `/${urlModel}/${document.id}`,
-          document,
-          isHomePage: false,
-        };
-      })
-      .filter(Boolean) as SiteMapEntry[];
-  },
     presetSource: {
         type: 'files',
         presetDirs: ['./.stackbit/presets']
