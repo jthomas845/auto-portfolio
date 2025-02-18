d38b550d7cc868762fa378b66dcc509285f93cbc
jthomas845 <54633261+jthomas845@users.noreply.github.com>
2025-02-18 11:37:13 -0500

    Update stackbit.config.ts -- siteMap fxn

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,4 @@
-import { defineStackbitConfig } from '@stackbit/types';
+import { defineStackbitConfig, SiteMapEntry } from '@stackbit/types';
 import { GitContentSource } from '@stackbit/cms-git';
 import { allModels } from './.stackbit/models';
 
@@ -9,7 +9,7 @@ const config = defineStackbitConfig({
     contentSources: [
         new GitContentSource({
             rootPath: __dirname,
-            contentDirs: ['content'],
+            contentDirs: ["content"],
             models: [
             {
               name: "Page",
@@ -32,6 +32,36 @@ const config = defineStackbitConfig({
         { name: "page", type: "page", urlPath: "/{slug}" },
         { name: "post", type: "page", urlPath: "/blog/{slug}" }
     ],
+    siteMap: ({ documents, models }) => {
+    // 1. Filter all page models
+    const pageModels = models.filter((m) => m.type === "page")
+
+    return documents
+      // 2. Filter all documents which are of a page model
+      .filter((d) => pageModels.some(m => m.name === d.modelName))
+      // 3. Map each document to a SiteMapEntry
+      .map((document) => {
+        // Map the model name to its corresponding URL
+        const urlModel = (() => {
+            switch (document.modelName) {
+                case 'Page':
+                    return 'otherPage';
+                case 'Blog':
+                    return 'otherBlog';
+                default:
+                    return null;
+            }
+        })();
+
+        return {
+          stableId: document.id,
+          urlPath: `/${urlModel}/${document.id}`,
+          document,
+          isHomePage: false,
+        };
+      })
+      .filter(Boolean) as SiteMapEntry[];
+  }
     presetSource: {
         type: 'files',
         presetDirs: ['./.stackbit/presets']
